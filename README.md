78cd39870c6afaf7e2138b6b34f60f72531ae8ee
Brautantoine <antoine.braut@gmail.com>
2019-05-18 01:33:00 +0200

    Fisrt commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # instantFlashHelper
 I got myself tired to always drag and drop .bin file after compiling it from Mbed so i made this ...
+
+If you want to use it just call it and pass the path where to flash your target:
+```sh
+
+./instantFlashHelper /media/user/cardName
+
+```
+
+> Take care that the script will move all the `.bin` you will download to the path you've specified. So please take care to what you download while using it.
+
+If you just want to copy the `.bin` instead of moving it replace the `mv` command by `cp` (maybe i'll make it as a built-in option on day ...)
---
+++ instantFlashHelper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#########################################################################################
+#    A Script that automatically mv all the .bin you download to the specified dest     #
+#    Copyright (C) 2019  Antoine Braut                                                  #
+#                                                                                       #
+#    This program is free software: you can redistribute it and/or modify               #
+#    it under the terms of the GNU General Public License as published by               #
+#    the Free Software Foundation, either version 3 of the License, or                  #
+#    (at your option) any later version.                                                #
+#                                                                                       #
+#    This program is distributed in the hope that it will be useful,                    #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+#    GNU General Public License for more details.                                       #
+#                                                                                       #
+#    You should have received a copy of the GNU General Public License                  #
+#    along with this program. If not, see <https://www.gnu.org/licenses/>.              #
+#                                                                                       #
+# Contact : Antoine.braut@gmail.com                                                     #
+#                                                                                       #
+#########################################################################################
+
+mkdir -p .temp
+
+CARD_PATH=$1
+DL_PATH=${HOME}/Downloads
+LAST_LIST=./.temp/last.list
+CUR_LIST=./.temp/cur.list
+
+ls ${DL_PATH} > ${LAST_LIST}
+
+while true; do
+
+	ls ${DL_PATH} > ${CUR_LIST}
+	
+	NEW_BIN=$(echo $(diff ${LAST_LIST} ${CUR_LIST} | grep '^>.*\.bin$') | sed -e 's/> *//')
+
+	[  -z ${NEW_BIN} ] || echo mv ${DL_PATH}/${NEW_BIN}  ${CARD_PATH}
+
+	ls ${DL_PATH} > ${LAST_LIST}
+
+	sleep 2
+done
